44a97306c6164b99edca4f03e0403c98ff7bca61
Aleksandr <loner.moriarty@gmail.com>
2023-03-12 20:33:40 +0300

    db

--- server/index.js
+++ server/index.js
@@ -1,10 +1,21 @@
 import * as dotenv from 'dotenv'
 dotenv.config()
 import express from 'express'
+import sequelize from './db.js'
 
 const app = express()
 app.use(express.json())
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+const start = async () => {
+    try {
+        await sequelize.authenticate()
+        await sequelize.sync()
+        app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+start()
