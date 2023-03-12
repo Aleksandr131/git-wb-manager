70a38964eb568e298ea8d427c2cbb2d3638f5e8a
Aleksandr <loner.moriarty@gmail.com>
2023-03-12 16:19:16 +0300

    first commit

---
+++ server/index.js
@@ -0,0 +1,10 @@
+import * as dotenv from 'dotenv'
+dotenv.config()
+import express from 'express'
+
+const app = express()
+app.use(express.json())
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
