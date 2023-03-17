69d5d94dd744be34ce94e55f2bbf09fe6532e41a
Aleksandr <loner.moriarty@gmail.com>
2023-03-17 10:39:51 +0300

    add host

--- scrapper/index.js
+++ scrapper/index.js
@@ -1,1 +1,16 @@
-////
+import dotenv from 'dotenv'
+dotenv.config()
+import axios from 'axios'
+
+const $authHost = axios.create({
+    baseURL: process.env.API_URL
+})
+
+const authInterceptor = config => {
+    config.headers.authorization = process.env.TOKEN
+    return config
+}
+
+$authHost.interceptors.request.use(authInterceptor)
+
+export default $authHost
